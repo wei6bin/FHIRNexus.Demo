77d2fca9c8fb3cedd996e761a1cae7ef7236b2e8
Bin Wei <wei.bin@synapxe.sg>
2024-11-22 23:04:01 +0800

    the extension to store at main table not works at data field level

--- Fhir.Relational.Demo2/Data/PatientEntity.cs
+++ Fhir.Relational.Demo2/Data/PatientEntity.cs
@@ -10,6 +10,11 @@ public partial class PatientEntity
     [FhirExtensionMapping("http://hl7.org/fhir/StructureDefinition/patient-animal")]
     public PatientAnimal? Animal { get; set; }
 
+    [FhirExtensionMapping("http://hl7.org/fhir/StructureDefinition/patient-birthTime")]
+    public DateTimeEntity? BirthTime { get; set; }
+
+    public List<CommunicationComponent> Communication { get; set; } = [];
+
     [FhirType("Extension", IsResource = false)]
     public class PatientAnimal : BaseOwnedEntity
     {
@@ -21,6 +26,35 @@ public partial class PatientEntity
 
         [FhirExtensionMapping("genderStatus")]
         public CodeableConceptSlimEntity GenderStatus { get; set; }
+
+        [FhirType("CodeableConcept", "http://hl7.org/fhir/StructureDefinition/CodeableConcept", IsResource = false)]
+        public class CodeableConceptSlimEntity : BaseOwnedEntity
+        {
+            public string? Text { get; set; }
+
+            public CodingEntity? Coding { get; set; }
+        }
+    }
+
+    [FhirType("Patient#Communication", IsNestedType = true)]
+    public class CommunicationComponent : BackboneEntity
+    {
+        public CodeableConceptSlimEntity? Language { get; set; }
+
+        [FhirExtensionMapping("http://hl7.org/fhir/StructureDefinition/patient-proficiency")]
+        public Proficiency? Proficiency { get; set; }
+
+        public bool? Preferred { get; set; }
+    }
+
+    [FhirType("Extension", IsResource = false)]
+    public class Proficiency : BaseOwnedEntity
+    {
+        [FhirExtensionMapping("level")]
+        public CodeableConceptSlimEntity Level { get; set; }
+
+        [FhirExtensionMapping("type")]
+        public CodeableConceptSlimEntity Type { get; set; }
 
         [FhirType("CodeableConcept", "http://hl7.org/fhir/StructureDefinition/CodeableConcept", IsResource = false)]
         public class CodeableConceptSlimEntity : BaseOwnedEntity
