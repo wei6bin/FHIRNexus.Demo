fcef90f242f3d21864a7ad36429586c1178cc125
wei6bin <wei.bin@synapxe.sg>
2025-06-06 11:19:58 +0800

    Upgrade dependencies and add PatientHandler class
    Updated package references in `Synapxe.FhirWebApi.NoSQL.csproj` from version `4.3.0` to `4.3.1` for several dependencies including SQL Server and OpenTelemetry.
    
    Added a new file `PatientHandler.cs` with the `PatientHandler` class, which includes the `ValidateNoAppointmentConflictAsync` method to process FHIR resources and handle patients named "Jack".

---
+++ Synapxe.FhirWebApi.NoSQL/Handlers/PatientHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Hl7.Fhir.Model;
+using Ihis.FhirEngine.Core;
+using Task = System.Threading.Tasks.Task;
+
+namespace Synapxe.FhirWebApi.NoSQL.Handlers;
+
+[FhirHandlerClass(AcceptedType = nameof(Patient))]
+public class PatientHandler
+{
+    [FhirHandler("PostInteraction", HandlerCategory.PostInteraction, FhirInteractionType.SearchType)]
+    public Task ValidateNoAppointmentConflictAsync(IFhirContext context, CancellationToken cancellationToken)
+    {
+        var searchResult = context.Response.TakeAllResources().FirstOrDefault();
+        foreach (var resource in (searchResult?.Resource as Bundle)?.Entry)
+        {
+            // Assuming resource is of type ResourceProxy and has a Resource property
+            if (resource.Resource is not Patient patient || patient.Name == null) continue;
+
+            var hasJack = patient.Name.Any(n =>
+                n.Text != null && n.Text.Contains("Jack", StringComparison.OrdinalIgnoreCase));
+            if (!hasJack)
+            {
+                continue;
+            }
+
+            context.Response.AddResource(patient);
+        }
+
+        context.Response.StatusCode = HttpStatusCode.OK;
+
+        return Task.CompletedTask;
+    }
+}
